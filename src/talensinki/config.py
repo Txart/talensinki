0ebe3f331cfe3779cfc0101168c5da5baa85b9c2
txart <iurzainki@gmail.com>
2025-08-19 07:57:41 +0200

    fix bug with database file structure

--- src/talensinki/config.py
+++ src/talensinki/config.py
@@ -61,7 +61,7 @@ AVAILABLE_EMBEDDING_MODELS = [
 
 PDF_FOLDER = Path("./data/pdfs")
 OLLAMA_LOCAL_URL = "http://localhost:11434"
-VECTOR_DATABASE_FILEPATH = Path("./data/chroma_database")
+VECTOR_DATABASE_FILEPATH = Path("./data/databases/chroma_database")
 VECTOR_DATABASE_COLLECTION_NAME = "PDF_collection"
 
 
